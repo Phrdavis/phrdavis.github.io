8a12c5f80d80cdfb1717705efd3b0e0febaf15a3
Davi Pinheiro de Souza <105741181+Phrdavis@users.noreply.github.com>
2024-07-16 20:49:32 -0300

    Update exibeProfissionais.js

--- js/exibeProfissionais.js
+++ js/exibeProfissionais.js
@@ -9,12 +9,12 @@ cargos.set('Desenvolvedor Python', 'Criação de interface gráfica para sistema
 //                          funcao, inicio, fim, local, cargosHtml
 profissao.exibirProfissional("Estagiário de Suporte em T.I", "Ago. 2021", "Ago. 2023", "Prefeitura de São Francisco do Sul", cargos)
 
-
+var profissao = new Profissional();
 var cargos = new Map();
 
-cargos.set('Desenvolvedor Web', 'Criação de novas interfaces para sistema web próprio da empresa dentro do setor de PCO (Planejamento e Controle de Operação), manuteção e arquitetura do código, utilizando linguagens HTML, CSS, JavaScript, PHP e banco MySQL. Desenvolvimento e planejamento de documentação de projeto. Criação de telas interativas, dashboards e aplicação de novas funcionalidades ao site')
+cargos.set('Desenvolvedor Auxiliar', 'Criação de novas interfaces para sistema web próprio da empresa dentro do setor de PCO (Planejamento e Controle de Operação), manuteção e arquitetura do código, utilizando linguagens HTML, CSS, JavaScript, PHP e banco MySQL. Desenvolvimento e planejamento de documentação de projeto. Criação de telas interativas, dashboards e aplicação de novas funcionalidades ao site')
 
 
 //                          funcao, inicio, fim, local, cargosHtml
-profissao.exibirProfissional("Estagiário de Suporte em T.I", "Ago. 2021", "Ago. 2023", "Prefeitura de São Francisco do Sul", cargos)
+profissao.exibirProfissional("Desenvolvedor Web, "Ago. 2023", "Atualmente", "Logimodal Operações Logísticas LTDA", cargos)
 
