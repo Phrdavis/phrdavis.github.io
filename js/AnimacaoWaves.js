064bdee0cd6e6cfe0b733b94caf019501d44fb56
davi.psouza <davi.psouza@totvs.com.br>
2025-05-08 13:21:23 -0300

    fix: Ajustes de Layout para os cards e ajuste na animação das waves do footer

--- js/AnimacaoWaves.js
+++ js/AnimacaoWaves.js
@@ -4,7 +4,7 @@ class AnimacaoWaves{
 
         var divWaves = document.getElementById("waves");
 
-        divWaves.innerHTML += `<img class="img img-fluid position-absolute ${wave}" style="bottom: 0%; z-index: 0;" src="images/Union.png" alt="${wave}">`;
+        divWaves.innerHTML += `<img class="img img-fluid position-absolute ${wave}" style="bottom: 100%; z-index: 0;" src="images/Union.png" alt="${wave}">`;
 
     }
 
